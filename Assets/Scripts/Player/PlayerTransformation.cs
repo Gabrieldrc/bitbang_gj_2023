b31d4a90e810d12fca3ba747c736cb126f744cb4
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-07 12:42:35 -0300

    Transformacion con grito mariachi

--- Assets/Scripts/Player/PlayerTransformation.cs
+++ Assets/Scripts/Player/PlayerTransformation.cs
@@ -6,33 +6,39 @@ public class PlayerTransformation : MonoBehaviour
 	[SerializeField] private Sprite _trashCanTransform, _normal;
 	[SerializeField] private float _tranformationTime;
 	[SerializeField] private SpriteRenderer _currentSprite;
-
+	[SerializeField] private AudioClip _gritoSapucai;
+	private AudioSource _aSource;
 	private void Start()
 	{
 		_currentSprite.sprite = _normal;
+		_aSource = GetComponentInParent<AudioSource>();
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("PowerTrash"))
 		{
-			ActivatePower();
+			StartCoroutine(CO_UsePower());
 		}
 	}
 
 	private void ActivatePower()
 	{
-		StartCoroutine(CO_UsePower());
+		Debug.Log("Trash man llama");
+		_currentSprite.sprite = _trashCanTransform;
+		_aSource.PlayOneShot(_gritoSapucai);
 	}
-
-
+	private void DeactivatePower()
+	{
+		_currentSprite.sprite = _normal;
+		Debug.Log("Normal man llama");
+	}
+	
 
 	private IEnumerator CO_UsePower()
 	{
-		Debug.Log("Trash man llama");
-		_currentSprite.sprite = _trashCanTransform; 
+		ActivatePower();
 		yield return new WaitForSeconds(_tranformationTime);
-		_currentSprite.sprite = _normal;
-		Debug.Log("Normal man llama");
+		DeactivatePower();
 	}
 }
