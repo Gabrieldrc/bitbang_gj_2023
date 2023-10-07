7aea19f7aae6e92f402633d3c7be1930bbb992b0
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-07 12:49:11 -0300

    Trash Can Power Sound Effect`
    Creado el sound effect para el super poder de transformarse en un tacho.

--- Assets/Scripts/SuperPower.cs
+++ Assets/Scripts/SuperPower.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class SuperPower : MonoBehaviour
 {
+	[SerializeField] private AudioClip _soundEffect;
+	private AudioSource _aSource;
+
+
+	private void Start()
+	{
+		_aSource = GetComponent<AudioSource>();
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (!collision.CompareTag("Player")) return;
+		UsePower();
+	}
+
+	private void UsePower()
+	{
+		StartCoroutine(CO_UsePowerEffect());
+		
+	}
+	IEnumerator CO_UsePowerEffect()
+	{
+		_aSource.PlayOneShot(_soundEffect);
+		GetComponent<SpriteRenderer>().enabled = false;
+		GetComponent<BoxCollider2D>().enabled = false;
+
+		while (_aSource.isPlaying)
+		{
+			yield return null;
+		}
 		Destroy(gameObject);
 	}
 }
