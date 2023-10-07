67480340d28859d82655e281626a085db4bdc32c
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-10-07 14:16:49 -0300

    Level, enemy, score, Live

--- Assets/Scripts/WaterDrops/RainDrop.cs
+++ Assets/Scripts/WaterDrops/RainDrop.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class RainDrop : MonoBehaviour
 {
 	[SerializeField, Range(0,1f)] protected float _speed;
-	[SerializeField] protected float _score;
+	[SerializeField] protected float _live;
+	[SerializeField] private FloatValueSO _liveSO;
 	[SerializeField] protected Rigidbody2D _rb2d;
 	protected void Move()
 	{
@@ -13,5 +14,12 @@ public class RainDrop : MonoBehaviour
 	protected void OnEnable()
 	{
 		Move();
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		_liveSO.value += _live;
+		_liveSO.Notify();
+		Destroy(gameObject);
 	}
 }
