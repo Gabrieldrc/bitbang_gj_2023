810033e0c12e07067f8b7f10e0e81a3f985da120
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-08 12:55:28 -0300

    Efecto de particulas en el tacho

--- Assets/Scripts/TrashcanBonus.cs
+++ Assets/Scripts/TrashcanBonus.cs
@@ -1,19 +1,24 @@
+using System.Collections;
+
 using UnityEngine;
 
 public class TrashcanBonus : MonoBehaviour
 {
-    [SerializeField] private int _score = 500;
+	[SerializeField] private int _score = 500;
+	[SerializeField] private GameObject _particles;
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.CompareTag("Enemy"))
-        {
-            var otherEnemy = col.GetComponent<Enemy>();
-            if (!otherEnemy.IsAlive)
-            {
-                GameManager.instance.AddScore(_score);
-                Destroy(otherEnemy.gameObject);
-            }
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.CompareTag("Enemy"))
+		{
+			var otherEnemy = col.GetComponent<Enemy>();
+			if (!otherEnemy.IsAlive)
+			{
+				GameManager.instance.AddScore(_score);
+				Destroy(otherEnemy.gameObject);
+				_particles.SetActive(true);
+			}
+		}
+	}
+
 }
